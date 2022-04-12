51d1cef6fab53a620c009e93480093f56f4d3910
Kouhei <{mkkhiti.iltill@gmail.com}>
2022-04-12 18:24:28 +0900

    Another method supplemented

--- index.js
+++ index.js
@@ -5,8 +5,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
     for (let i = 0; i < square.length; i++) {
         square[i].addEventListener("mouseover", () => {
-            square[i].classList.toggle("animate");
-            // window.alert("遊ぶな！！");
+            if (square[i].classList.contains("animate") === false) {
+                square[i].classList.add("animate");
+                // window.alert("遊ぶな！！");
+            } else {
+                square[i].classList.remove("animate");
+            }
+
+            // if文消してこっちでもいける
+            // square[i].classList.add("animate");
         });
     }
 });
