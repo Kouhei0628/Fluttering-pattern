7e60bbd071b9a997b0a9480b4856d7b3a97d94bb
Kouhei <{mkkhiti.iltill@gmail.com}>
2022-04-17 22:53:45 +0900

    アニメーション改善、送信ボタン機能追加etc.

--- index.js
+++ index.js
@@ -7,13 +7,9 @@ document.addEventListener("DOMContentLoaded", function() {
         square[i].addEventListener("mouseover", () => {
             if (square[i].classList.contains("animate") === false) {
                 square[i].classList.add("animate");
-                // window.alert("遊ぶな！！");
             } else {
                 square[i].classList.remove("animate");
             }
-
-            // if文消してこっちでもいける
-            // square[i].classList.add("animate");
         });
     }
 });
